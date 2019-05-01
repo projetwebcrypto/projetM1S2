00e0d47f6e5aa92d0cc7d5a9a7c3c074a2969e08
atobodel <alexandre-tobo-delaleu@etud.univ-tln.fr>
2019-05-01 23:23:58 +0200

    9: Réception des données en JSON après requête REST: Mise en place basique

--- webCryptoServeur/src/main/java/fr/univtln/groupe1/webCrypto/REST/serveurREST.java
+++ webCryptoServeur/src/main/java/fr/univtln/groupe1/webCrypto/REST/serveurREST.java
@@ -1,5 +1,9 @@
 package fr.univtln.groupe1.webCrypto.REST;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonWriter;
@@ -13,10 +17,46 @@ import java.util.Map;
 @Path("/moncoffre")
 @Produces({"application/json"})
 public class serveurREST {
-    @GET
-    public List<Json> getBDDTriplets() {
-        return null;
+
+    /**
+     * Méthode qui retourne tous les triplets de la BDD nomdb
+     * associé au login
+     * @return String ???
+     */
+    @POST
+    @Path("/login")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String getBDDTriplets(String listJson) {
+        System.out.println(listJson);
+        //JSONArray listJS = new JSONArray(listJson);
+        //JSONArray listJS = (JSONArray) new JSONTokener(listJson).nextValue();
+        //JSONObject request = new JSONObject(listJS.getJSONObject(0));
+        JSONObject request = new JSONObject(listJson);
+        System.out.println(request);
+        // methodes de traitement
+        // faire JSONArray
+
+        return request.toString();
     }
+//    @PUT
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    public String test(String test) {
+//        System.out.println(test);
+//        System.out.println("Apres test");
+//        return "coucou";
+//    }
     // reception, envoie de json triplet
     // reception de json avec login, nomdb
+
+    /**
+     * Méthode qui insert les triplets reçus dans la BDD nomdb
+     * associé au login
+     */
+    @POST
+    public void postBDDTriplets(/*Something*/) {
+        /*
+        format: {"login": "X", "nomdb": "Y", "triplets": [...]}
+         */
+
+    }
 }
