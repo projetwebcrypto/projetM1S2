2719f27e6ec17bab149df6c86d0239ace7ec1a36
Biglycan <xrichard455@etud.univ-tln.fr>
2019-05-02 13:34:29 +0200

    2 : Recuperer des donnees JSON : Mise en place de la liaison Client-Serveur

--- webCryptoClient/js/client.js
+++ webCryptoClient/js/client.js
@@ -2,34 +2,36 @@ $(document).ready(function(){
 
   console.log("nimportequoi2");
   $("#DL").click(function(){
-
+  data = {"login":'log',"bd":'passwords'};
 
     $.ajax({
-      type:'GET',
-      url:'http://0.0.0.0:8080/myapp/chiens',// url serveur depot
-      data:{"{ 'login' : log, 'bd' : passwords}"},
-      dataType:'json',
-      //beforeSend :function(){
-      success:function(res){
+      type:'POST',
+      url:'http://192.168.99.100:8080/monCoffre/moncoffre/login',
+      data:JSON.stringify(data),
+      dataType:'text',
+      contentType:'application/json',// OBLIGATOIRE !!!!!!!!!!!
+      // accepts: "*/*",
+      success:function(json,status){
         // crée la table html
+        // alert(json);
         var ch = '<table class="table"><thead><tr><th scope="col">#</th>'
-        ch += '<th scope="col">Site</th><th scope="Identifiants">Last</th>'
-        ch += '</tr></thead>';
-        console.log(res);
-        if (res.length > 0){
-          for (var i=0; i<res.length; i++){
-            // for each e€res e[i] devient un element de la table
-            ch += '<tbody><tr><th scope="row">' + i+1 + '</th><td>'
-            ch += '<li class="list-group-item">' + res[i].site+'</td>';
-            ch += '<td>' + res[i].crypto + '</td></tr>'
+        ch = ch + '<th scope="col">Site</th><th scope="identifiants">Crypto</th>'
+        ch = ch + '</tr></thead>';
+        var myobj = JSON.parse(json);
+        if (myobj.triplets.length > 0){
+          for (var i=0; i<myobj.triplets.length; i++){
+            // for each e€json e[i] devient un element de la table
+            ch += '<tbody><tr><th scope="row">' + i + '</th><td>'
+            ch += '<li class="list-group-item">' + myobj.triplets[i].site+'</td>';
+            ch += '<td>' + myobj.triplets[i].crypto + '</td></tr>'
           }
           // fermeture des balise
-          ch += '</tbody></table>';
+          ch = ch+'</tbody></table>';
         }
 
         $('body').append(ch);
       },
-      error:function(){$('bd').html("");}
+      error:function(data,status){console.log("error POST"+data+" et "+status);}
     })
   })
 });
